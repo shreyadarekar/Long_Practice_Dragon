0c56d9a0f91581066a0e4d21bf88e745a26fbd6a
Anthony Febles <anthonyfebles21@outlook.com>
2023-05-18 18:56:11 -0400

    phase 1 and 2 done

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,26 @@
-// Your code here
+class Dragon {
+  constructor(name, color){
+    this.name = name
+    this.color = color
+  }
+
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragonNames){
+    return dragonNames.map(dr => dr.name)
+  }
+}
+
+
+const puff = new Dragon("Puff", "green");
+const toothless = new Dragon("Toothless", "black");
+console.log(Dragon.getDragons(puff, toothless));
+
+
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,42 @@
-// Your code here
+const Dragon = require("./dragon")
+
+class FriendlyDragon extends Dragon {
+    constructor (name, color, lifeGoals = [], friend){
+        super(name,color);
+        this.lifeGoals = lifeGoals,
+        this.friend = friend
+    }
+
+    hasLifeGoals(){
+        for (let i =0; i< this.lifeGoals.length; i++){
+            let lifeGoals = this.lifeGoals[i]
+            console.log(`${this.name} likes to ${lifeGoals}`)
+        
+        }
+    }
+
+    helpsPeople(){
+        return `${this.name} helps their friend ${this.friend}`
+    }
+}
+
+const puff = new FriendlyDragon(
+    "Puff",
+    "green",
+    [
+      "live by the sea",
+      "frolick in the autumn mist",
+      "help small children"
+    ],
+    "Jackie Paper"
+  );
+  console.log(puff);
+  console.log(puff.breathesFire()); //Friendly dragons can still breathe fire!
+  puff.hasLifeGoals();
+  console.log(puff.helpsPeople());
+
+
+
+
+
+module.exports = FriendlyDragon
